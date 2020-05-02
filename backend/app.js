8d281c22802d23ea8b866d90eca8d68a4248c539
Sok Ang Ho <hosokang3311@gmail.com>
2020-05-02 13:29:06 +1000

    Add bootstrap and jquery

--- backend/app.js
+++ backend/app.js
@@ -7,19 +7,29 @@ const errorHandlers = require('./handlers/errorHandlers');
 
 const app = express();
 
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// Servers up static files in public folder
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
 // Takes the raw requests and turns them into usable properties on req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Routes handling
 app.use('/', routes);
 
+// Error handling for dev environment
 if (app.get('env') === 'development') {
   app.use(errorHandlers.developmentErrors);
 }
 
+// Error handling for prod environment
 app.use(errorHandlers.productionErrors);
 
 module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hey! It works');
+  res.render('layout');
 });
 
 module.exports = router;
