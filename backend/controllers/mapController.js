1944a213122e04e4fce1584ccb6c036f2dafe79c
Sok Ang Ho <hosokang3311@gmail.com>
2020-05-02 22:55:49 +1000

    Get routing between 2 locations to work again

--- backend/controllers/mapController.js
+++ backend/controllers/mapController.js
@@ -1,4 +1,5 @@
 const { Client, Status } = require('@googlemaps/google-maps-services-js');
+const client = new Client({});
 
 exports.homePage = async (req, res) => {
   const orig = req.query.orig;
@@ -7,17 +8,43 @@ exports.homePage = async (req, res) => {
   const origDetails = orig && (await getPlaceDetails(orig));
   const destDetails = dest && (await getPlaceDetails(dest));
 
+  // const routes = await getRoutes(
+  //   origDetails.geometry.location,
+  //   destDetails.geometry.location,
+  //   'DRIVING'
+  // );
+
   res.render('index', {
     orig,
     dest,
     origAddress: origDetails.formatted_address,
     destAddress: destDetails.formatted_address,
+    // routes,
   });
 };
 
-getPlaceDetails = async (placeId) => {
-  const client = new Client({});
+// getRoutes = async (orig, dest, mode) => {
+//   try {
+//     const routes = await client.directions({
+//       params: {
+//         origin: orig,
+//         destination: dest,
+//         mode,
+//         key: process.env.GOOGLE_MAP_KEY,
+//       },
+//     });
 
+//     if (routes.data.status === Status.OK) {
+//       return routes.data;
+//     } else {
+//       console.log(routes.data.error_message);
+//     }
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+getPlaceDetails = async (placeId) => {
   try {
     const placeDetails = await client.placeDetails({
       params: {
--- backend/public/js/modules/map.js
+++ backend/public/js/modules/map.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export function initMap() {
   // Initilise map to Melbourne location
   const map = new google.maps.Map(document.getElementById('map'), {
@@ -5,18 +7,18 @@ export function initMap() {
     zoom: 8,
   });
 
-  // new AutocompleteDirectionsHandler(map);
+  // Draw routes on the map
+  drawRoutes(map);
 }
 
+// Google Map places auto completion on input
 export function autocomplete(textInput, placeIdInput) {
   if (!textInput) return;
 
   const dropdown = new google.maps.places.Autocomplete(textInput);
-
   dropdown.addListener('place_changed', () => {
     const place = dropdown.getPlace();
     placeIdInput.value = place.place_id;
-    console.log(place); //"ChIJP3Sa8ziYEmsRUKgyFmh9AQM"
   });
 
   textInput.addEventListener('keydown', (e) => {
@@ -24,68 +26,23 @@ export function autocomplete(textInput, placeIdInput) {
   });
 }
 
-function AutocompleteDirectionsHandler(map) {
-  this.map = map;
-  this.originPlaceId = null;
-  this.destinationPlaceId = null;
-  this.travelMode = 'DRIVING';
-  this.directionsService = new google.maps.DirectionsService();
-  this.directionsRenderer = new google.maps.DirectionsRenderer();
-  this.directionsRenderer.setMap(map);
+function drawRoutes(map) {
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  directionsRenderer.setMap(map);
 
-  const originInput = document.getElementById('origin-input');
-  const destinationInput = document.getElementById('destination-input');
-  // let modeSelector = document.getElementById()
-
-  // Setup origin and destination auto complete inputs
-  const originAutocomplete = new google.maps.places.Autocomplete(originInput);
-  originAutocomplete.setFields(['place_id']);
-  const destinationAutocomplete = new google.maps.places.Autocomplete(destinationInput);
-  destinationAutocomplete.setFields(['place_id']);
-
-  this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
-  this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
-}
-
-AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function (autocomplete, mode) {
-  const me = this;
-  autocomplete.bindTo('bounds', this.map);
-
-  autocomplete.addListener('place_changed', function () {
-    const place = autocomplete.getPlace();
-
-    if (!place.place_id) {
-      window.alert('Please select an option from the dropdown list.');
-      return;
-    }
-
-    if (mode === 'ORIG') {
-      me.originPlaceId = place.place_id;
-    } else {
-      me.destinationPlaceId = place.place_id;
-    }
-    me.route();
-  });
-};
-
-AutocompleteDirectionsHandler.prototype.route = function () {
-  if (!this.originPlaceId || !this.destinationPlaceId) {
-    return;
-  }
-  const me = this;
-
-  this.directionsService.route(
+  directionsService.route(
     {
-      origin: { placeId: this.originPlaceId },
-      destination: { placeId: this.destinationPlaceId },
+      origin: { placeId: document.getElementById('orig-id').value },
+      destination: { placeId: document.getElementById('dest-id').value },
       travelMode: 'DRIVING',
     },
     function (res, status) {
       if (status === 'OK') {
-        me.directionsRenderer.setDirections(res);
+        directionsRenderer.setDirections(res);
       } else {
-        window.alert('Directions request failed due to ' + status);
+        console.log(status);
       }
     }
   );
-};
+}
